32a6dbc2b6c8112d1e6d368f1988ebd143a1933d
marcos  tavares <marcos.vinicius@itarget.com.br>
2024-01-28 20:49:17 -0300

    Criação de conver decimal para binário usando lógica de pilha

--- js/pilhas.js
+++ js/pilhas.js
@@ -69,3 +69,23 @@
 // pilha.pop()
 
 // console.log(pilha.isEmpty())
+
+function dec2Bin(decNumber) {
+    var restStack = [],
+    rest,
+    binaryString = ''
+
+    while(decNumber > 0) {
+        rest = Math.floor(decNumber % 2)
+        restStack.push(rest)
+        decNumber = Math.floor(decNumber / 2)
+    }
+
+    while(restStack.length > 0) {
+        binaryString += restStack.pop().toString()
+    }
+
+    return binaryString;
+}
+
+console.log(dec2Bin(25))
