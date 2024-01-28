f747ea1076e6d5e96c3d10b5cbafa4745abe6e19
marcos  tavares <marcos.vinicius@itarget.com.br>
2024-01-28 20:34:36 -0300

    Comentando a classe

--- js/pilhas.js
+++ js/pilhas.js
@@ -1,71 +1,71 @@
 // LIFO (last in firt out)
 
-function Stack(){
+// function Stack(){
 
-    var items = []
+//     var items = []
 
-    this.push = function(element){
-        // adiciona um novo item a pilha
-        items.push(element)
-    }
+//     this.push = function(element){
+//         // adiciona um novo item a pilha
+//         items.push(element)
+//     }
 
-    this.pop = function(){
-        // remove o item do topo da pilha
-        return items.pop()
-    }
+//     this.pop = function(){
+//         // remove o item do topo da pilha
+//         return items.pop()
+//     }
 
-    this.peek = function(){
-        // devolve o elemento que está no topo da pilha
-        return items[items.length - 1]
-    }
+//     this.peek = function(){
+//         // devolve o elemento que está no topo da pilha
+//         return items[items.length - 1]
+//     }
 
-    this.isEmpty = function(){
-        // informa se a pilha está vazia ou não
-        return items.length === 0
-    }
+//     this.isEmpty = function(){
+//         // informa se a pilha está vazia ou não
+//         return items.length === 0
+//     }
 
-    this.clear = function(){
-        // limpa a pilha
-        items = []
-    }
+//     this.clear = function(){
+//         // limpa a pilha
+//         items = []
+//     }
 
-    this.size = function(){
-        // informar o tamanho da pilha
-        return items.length
-    }
+//     this.size = function(){
+//         // informar o tamanho da pilha
+//         return items.length
+//     }
 
-    this.print = function(){
-        // imprime a pilha no console
-        console.log(items.toString())
-    }
+//     this.print = function(){
+//         // imprime a pilha no console
+//         console.log(items.toString())
+//     }
 
-}
+// }
 
-var pilha = new Stack()
+// var pilha = new Stack()
  
-console.log(pilha.isEmpty())
+// console.log(pilha.isEmpty())
 
-pilha.push(5)
-pilha.print()
+// pilha.push(5)
+// pilha.print()
 
-pilha.push(8)
-pilha.print()
+// pilha.push(8)
+// pilha.print()
 
-pilha.push(11)
-pilha.print()
+// pilha.push(11)
+// pilha.print()
 
-pilha.push(15)
-pilha.print()
+// pilha.push(15)
+// pilha.print()
 
-pilha.pop()
-pilha.print()
+// pilha.pop()
+// pilha.print()
 
-pilha.pop()
-pilha.print()
+// pilha.pop()
+// pilha.print()
 
-pilha.pop()
-pilha.print()
+// pilha.pop()
+// pilha.print()
 
-pilha.pop()
+// pilha.pop()
 
-console.log(pilha.isEmpty())
+// console.log(pilha.isEmpty())
