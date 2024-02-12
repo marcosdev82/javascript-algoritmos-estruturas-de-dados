3cb3cbf27ec8215b40d01fab0127dc2c30aabad9
marcos  tavares <marcos.vinicius@itarget.com.br>
2024-02-12 10:22:00 -0300

    Criando os métodos

---
+++ js/filas.js
@@ -0,0 +1,27 @@
+function Queue(){
+    var items = []
+
+    this.enqueue = function(){
+        // adiciono um novo item
+    }
+
+    this.dequeue = function(){
+        // remove um item
+    }
+
+    this.front = function(){
+        // retorna o primeiro elemento da fila
+    }
+
+    this.isEmpty = function(){
+        // verifica se a fila está vazia
+    }
+
+    this.size = function(){
+        // retorna o tamanho da fila
+    }
+
+    this.print = function(){
+        // imprime a fila no console
+    }
+}
