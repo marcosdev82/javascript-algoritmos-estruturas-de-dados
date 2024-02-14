d4ebf2f7d760478cd7a6a7ae54dec38f70954895
mikek182 <marcosvinicius@geekwork.com.br>
2024-02-14 08:57:57 -0300

    Criação da classe ListasLigadas

---
+++ js/listas-ligadas.js
@@ -0,0 +1,41 @@
+function ListasLigadas() {
+    var Node = function(element) {
+        this.element = element
+        this.next = null
+    }
+
+    var length = 0
+    var head = null
+
+    this.append = function(element) {
+        //adiciona um elemento no final da list
+    }
+
+    this.insert = function(position, element) {
+        //adiciona um elemento em uma posição especfica
+    }
+
+    this.removeAt = function(position) {
+        //remove elemento de uma posição especifica
+    }
+
+    this.remove = function(element) {
+        //remove o elemento element
+    }
+
+    this.indexOf = function(element) {
+        //retorna a posição do elemento
+    }
+
+    this.isEmpty = function() {
+        //retorna se está vazia ou não
+    }
+
+    this.size = function() {
+        //retorna o tamanho da instancia
+    }
+
+    this.print = function() {
+        //imprime no console
+    }
+}
