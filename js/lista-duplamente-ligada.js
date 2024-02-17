0d3f28ac35f8c39c2288e704fee598b714317e9f
marcos  tavares <marcos.vinicius@itarget.com.br>
2024-02-17 17:06:28 -0300

    Implementação do método insert

--- js/lista-duplamente-ligada.js
+++ js/lista-duplamente-ligada.js
@@ -31,7 +31,41 @@ function ListaDuplamenteLigada() {
     }
 
     this.insert = function(position, element) {
-       
+       if (position > 0 && position <= length) {
+            var node = new Node(element),
+            current = head,
+            previous,
+            index = 0
+
+            if (position === 0) {
+                if (!head) {
+                    head = node
+                    tail = node
+                } else {
+                    node.next = current
+                    current.prev = node
+                    head = node
+                }
+            } else if (position === length) {
+                current = tail
+                current.next = node
+                node.prev = current
+                tail = node
+            } else {
+                while(index++ < position) {
+                    previous = current
+                    current.current.next
+                }
+                node.next = current
+                previous.next = node
+                current.prev = node
+                node.prev = previous
+            }
+            length++
+            return true
+       } else {
+            return false
+       }
     }
 
     this.removeAt = function(position) {
