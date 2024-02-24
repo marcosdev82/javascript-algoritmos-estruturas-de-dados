db0bed4fab6aaaa2c55a914ffb808900f7769e35
marcos  tavares <marcos.vinicius@itarget.com.br>
2024-02-24 19:16:40 -0300

    Criação dos métodos

--- js/conjuntos.js
+++ js/conjuntos.js
@@ -1,3 +1,29 @@
 function set() {
-    
+
+    var items = {}
+
+    this.add = function(value) {
+        // adiciona um novo item no conjunto
+    }
+
+    this.delete = function(value) {
+        // remove um valor do conjunto
+    }
+
+    this.has = function(value) {
+        // devolve se um valor existe ou não
+    }
+
+    this.clear = function() {
+        // remove todos os itens do conjunto
+    }
+
+    this.size = function() {
+        // retorna o tamanho do conjunto
+    }
+
+    this.values = function() {
+        // retorna um array com todos os valores do conjunto
+    }
+
 }
