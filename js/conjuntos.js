6f3f03163e31353e8578ee470c7d08c874851cf2
marcos  tavares <marcos.vinicius@itarget.com.br>
2024-02-24 19:29:58 -0300

    implentação do método values

--- js/conjuntos.js
+++ js/conjuntos.js
@@ -37,6 +37,12 @@ function set() {
 
     this.values = function() {
         // retorna um array com todos os valores do conjunto
+        var values = [],
+        keys = Object.keys(items)
+        for(var i = 0; i < keys.length; i++) {
+            values.push(items[keys[i]])
+        }
+        return values
     }
 
 }
